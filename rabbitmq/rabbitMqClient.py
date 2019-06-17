a1055d7028e63cb96ceb7cc896997344fc7f20f9
alessandro valentini <alessandro.valentini.90@gmail.com>
2019-06-17 22:48:33 +0200

    remove hard coded config

--- rabbitmq/rabbitMqClient.py
+++ rabbitmq/rabbitMqClient.py
@@ -1,19 +1,21 @@
 import pika
 
-
+host_name="localhost"
+queue_name="hello"
+routing_key_name="hello"
 
 def init():
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost'))
+        pika.ConnectionParameters(host=host_name))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue=queue_name)
     return connection
 
 def send():
     connection = init()
 
-    connection.channel().basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    connection.channel().basic_publish(exchange='', routing_key=routing_key_name, body='Hello World!')
     print(" [x] Sent 'Hello World!'")
     connection.close()
 
@@ -26,7 +28,7 @@ def receive():
         return body
 
     channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()

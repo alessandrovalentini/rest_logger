aa62dd0fe332d60057107cda24cc0b5c20cb555c
alessandro valentini <alessandro.valentini.90@gmail.com>
2019-06-20 23:42:55 +0200

    dequeue on separate thread

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ from logger import default_logger
 from rabbitmq import send
 from rabbitmq import receive
 from rabbitmq import rabbitMqClient
+import threading
 
 app = Flask(__name__)
 
-@app.route('/<arg>')
-def home(arg):
-    user = requests.getQueryParam('user')
-    logger =  default_logger.simpleLogger()
-    logger.warn("Request on " + arg)
-    return user
 
+from threading import Thread
+from time import sleep
 
-@app.route('/hello')
+@app.route('/')
 def index():
     return "Hello, World!"
+
+#@app.route('/<arg>')
+#def home(arg):
+#    user = requests.getQueryParam('user')
+#    logger =  default_logger.simpleLogger()
+#    logger.warn("Request on " + arg)
+#    return user
+
 
 @app.route('/send')
 def writeToQueue():
@@ -27,9 +32,13 @@ def writeToQueue():
     #receive.receive()
     return "message sent!"
 
-@app.route('/receive')
-def readFromQueue():
-    rabbitMqClient.receive()
-    return "message received"
+#@app.route('/receive')
+#def readFromQueue():
+#    rthreading.Thread(target=rabbitMqClient.receive).start()
+#    return "start receiving messages from queue..."
+
+if __name__ == "__main__":
+    threading.Thread(target=app.run).start()
+    threading.Thread(target=rabbitMqClient.receive).start()
 
 # http://localhost:5000/ciao?user=asasf
